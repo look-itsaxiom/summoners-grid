b287a9a502db331dd96e1845c061abd82b6e772a
Axiom <itsaxiom.dev@proton.me>
2025-08-18 01:36:17 -0700

    project init

---
+++ apps/game-client/src/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@summoners-grid/game-client" />
+    </div>
+  );
+}
+
+export default App;
---
+++ packages/game-engine/src/lib/game-engine.spec.ts
@@ -0,0 +1,7 @@
+import { gameEngine } from './game-engine.js';
+
+describe('gameEngine', () => {
+  it('should work', () => {
+    expect(gameEngine()).toEqual('game-engine');
+  });
+});
